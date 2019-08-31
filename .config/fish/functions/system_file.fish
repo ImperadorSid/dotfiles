d11b29c1adfbb5c82297f0394acd2defe8a4f964
Moisés Carvalho <moisescarv27@hotmail.com>
2019-08-31 15:30:01 -0300

    Revert to original output path

--- .config/fish/functions/system_file.fish
+++ .config/fish/functions/system_file.fish
@@ -4,7 +4,7 @@ function system_file
     set file_dir (dirname $file_path)
     set cur_dir $PWD
 
-    set out_dir /mnt/Dados/Arquivos/Downloads/Linux/Linux/system-files
+    set out_dir ~/Downloads/Linux/Linux/system-files
 
     s mkdir -p $file_dir
     s vim $file_path
