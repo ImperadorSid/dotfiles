ea8b8bf37eea0b6c4f09cfc33ec7cd47e10c3d3b
Moisés Carvalho <moisescarv27@hotmail.com>
2019-08-31 15:29:16 -0300

    Optimize git commands

--- .config/fish/functions/commit_repo.fish
+++ .config/fish/functions/commit_repo.fish
@@ -1,15 +1,12 @@
 #!/usr/bin/env fish
 
 function commit_repo
-    set cur_dir $PWD
     set out_dir $argv[1]
     
     read -p 'set_color green; echo -n \'Commit message\'; set_color normal; echo \': \'' msg
     if test -n $msg
-	cd $out_dir
-        g add .
-        g commit -m $msg
-	cd $cur_dir 
+        g -C $out_dir add .
+        g -C $out_dir commit -m $msg
     end
 
 end
