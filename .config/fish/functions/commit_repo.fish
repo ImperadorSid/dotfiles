997c948c37c9fbbe0709c0f4a1191e275a902c66
Moisés Carvalho <moisescarv27@hotmail.com>
2019-10-03 16:25:17 -0300

    Add auto push script

--- .config/fish/functions/commit_repo.fish
+++ .config/fish/functions/commit_repo.fish
@@ -7,6 +7,11 @@ function commit_repo
     if test -n $msg
         g -C $out_dir add .
         g -C $out_dir commit -m $msg
+
+	read -p 'set_color blue; echo -n \'Make push? \'; set_color normal; echo \'[Y/n] \'' choice
+	if test $choice != 'n'
+	  push_repo $out_dir
+	end
     end
 
 end
