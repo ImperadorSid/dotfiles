cb5f90cb819d0ac766f39b975157e65a42b1a1a6
Moisés Carvalho <moisescarv27@hotmail.com>
2019-10-03 16:27:34 -0300

    Add push script itself

---
+++ .config/fish/functions/push_repo.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+function push_repo
+  set target_dir $argv[1]
+
+  if not g -C $target_dir push
+      if not g -C $target_dir pull
+          for f in (g -C $target_dir diff --name-only --diff-filter=U)
+	      vim $target_dir/$f
+          end
+          commit_repo $target_dir
+      end
+      g -C $target_dir push
+  end
+end
