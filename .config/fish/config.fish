99a396d58e01eb43eeff39a18b3b0e0b03f6ae4a
Moisés Carvalho <moisescarv27@hotmail.com>
2019-11-26 12:40:56 -0300

    Add Node.JS binaries to PATH variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
-set -x PATH ~/.anaconda3/bin ~/.android-dev/flutter/bin ~/.android-dev/sdk/tools/bin ~/.android-dev/sdk/platform-tools ~/.android-dev/sdk/emulator/ $PATH
+set -x PATH ~/.anaconda3/bin ~/.android-dev/flutter/bin ~/.android-dev/sdk/tools/bin ~/.android-dev/sdk/platform-tools ~/.android-dev/sdk/emulator/ ~/.nodejs/bin $PATH
 set -x PYTHONPATH ~/.python-scripts
 
 alias a 's apt'
