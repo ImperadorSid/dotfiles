574d693d1ace682dd71d0e47d559a1bfeb509e69
Moisés Carvalho <moisescarv27@hotmail.com>
2019-11-30 12:20:42 -0300

    Update to use "super-vim" alias

--- .config/fish/functions/system_file.fish
+++ .config/fish/functions/system_file.fish
@@ -6,7 +6,7 @@ function system_file
     set out_dir ~/Downloads/Linux/Linux/system-files
 
     s mkdir -p $file_dir
-    s v $file_path
+    sv $file_path
 
     mkdir -p $out_dir/$file_dir
     s cp $file_path $out_dir/$file_dir
