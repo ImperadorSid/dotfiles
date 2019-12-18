4078a9dfd9922ec1cb4ba200613829b07e1f551b
Moisés Carvalho <moisescarv27@hotmail.com>
2019-12-18 00:29:53 -0300

    Remove PYTHONPATH environment variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,7 @@
 #!/usr/bin/env fish
 set -x PATH ~/.anaconda3/bin ~/.android-dev/flutter/bin ~/.android-dev/sdk/tools/bin ~/.android-dev/sdk/platform-tools ~/.android-dev/sdk/emulator/ ~/.nodejs/bin $PATH
-set -x PYTHONPATH ~/.python-scripts
 
-alias a 's apt'
+alias a 'apt'
 alias c 'clear; clear'
 alias g git
 alias jn 'jupyter-notebook'
