1561e891dbd7db79d160993aa2050dea0ee9c80b
Moisés Carvalho <moisescarv27@hotmail.com>
2020-03-23 18:20:51 -0300

    Remove node from PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
-set -x PATH ~/.anaconda3/bin ~/.android-dev/flutter/bin ~/.android-dev/sdk/cmdline-tools/latest/bin ~/.android-dev/sdk/platform-tools ~/.android-dev/sdk/emulator/ ~/.nodejs/bin $PATH
+set -x PATH ~/.anaconda3/bin ~/.android-dev/flutter/bin ~/.android-dev/sdk/cmdline-tools/latest/bin ~/.android-dev/sdk/platform-tools ~/.android-dev/sdk/emulator/ $PATH
 
 set -x dotfiles ~/Downloads/Linux/System/Linux/dotfiles
 set -x system_files ~/Downloads/Linux/System/Linux/system-files
