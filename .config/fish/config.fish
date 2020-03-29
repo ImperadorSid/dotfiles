010a11d82aa1288e47c4d01a4b418a1d35c2a670
Moisés Carvalho <moisescarv27@hotmail.com>
2020-03-29 11:36:48 -0300

    Add theme-related settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,11 +4,15 @@ set -x PATH ~/.anaconda3/bin ~/.android-dev/flutter/bin ~/.android-dev/sdk/cmdli
 set -x dotfiles ~/Downloads/Linux/System/Linux/dotfiles
 set -x system_files ~/Downloads/Linux/System/Linux/system-files
 
+set -g theme_display_date no
+set -g theme_display_nvm yes
+set -g theme_display_sudo_user yes
+set -g theme_color_scheme dracula
+
 alias a 'apt'
 alias c 'clear; clear'
 alias g 'git'
 alias jl 'jupyter lab'
-alias jn 'jupyter notebook'
 alias n 'npm'
 alias N 'node'
 alias p 'ipython'
