212354ddb4a6e963c49c786c36b073f78e01d07c
Moisés Carvalho <moisescarv27@hotmail.com>
2020-04-08 22:28:27 -0300

    Reduce $PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 # PATH variable
-set -x PATH ~/.local/anaconda3/bin ~/.local/flutter/bin ~/.local/android-sdk/cmdline-tools/latest/bin ~/.local/android-sdk/platform-tools ~/.local/android-sdk/emulator/ $PATH
+set -x PATH ~/.local/anaconda3/bin ~/.local/bin $PATH
 
 # System configuraton folders
 set -x repositories ~/Downloads/Linux/System/Repositories
@@ -24,15 +24,9 @@ alias l  'less -N'
 alias n  'npm'
 alias N  'node'
 alias p  'ipython'
-alias s  'sudo'
 alias sc 's systemctl'
 alias sv 's vim'
 alias t  'tree -C'
 alias v  'vim'
 alias x  'xclip -selection clipboard'
 
-# Abbreviations
-abbr se 'sed -nr \'s|||p\''
-
-# Change default mode
-fish_vi_key_bindings
