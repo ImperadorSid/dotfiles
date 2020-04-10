129c5b24ba0666dfbc90f2938a353a62f515c3a6
Moisés Carvalho <moisescarv27@hotmail.com>
2020-04-10 12:25:30 -0300

    Improve URI input handling

--- .config/fish/functions/json_cache.fish
+++ .config/fish/functions/json_cache.fish
@@ -2,29 +2,53 @@
 
 function json_cache
   # Parsing arguments
-  if not argparse -x 'f,c' -X 1 'f/force' 'c/clean' -- $argv
-    return
-  else if test -z $argv
-    echo 'Parameter required "URI" not specified'
-  end
+  argparse -n 'JSON Cache' -x 'f,c' -X 1 'f/force' 'c/clean' -- $argv
+  if test $status -ne 0; return 1; end
 
   # Set variables
   set cache_dir ~/.cache/fish_json
   set index_file $cache_dir/index.json
 
   if set -q _flag_f
+    if not __json_cache_check_uri $argv; return 2; end
     echo 'Flag F'
-    curl -s $argv
+
+    if not curl -s $argv
+      echo 'Download failed'
+      return 3
+    end
+
   else if set -q _flag_c
-    rm $cache_dir/.
+    rm $cache_dir/*
     echo '[]' > $index_file
 
     echo 'Cache directory reseted'
+
   else
+    if not __json_cache_check_uri $argv; return 2; end
     set next_slot (jq '. | length' $index_file)
 
-    a2 -q $argv -d $cache_dir -o $next_slot.json 
-    cat $cache_dir/$next_slot.json
+    if not a2 -q --allow-overwrite -d $cache_dir -o $next_slot.json (__json_cache_add_uri_prefix $argv)
+      echo 'Download failed'
+      return 3
+    end
+    jq '.' $cache_dir/$next_slot.json
+  end
+end
+
+function __json_cache_check_uri
+  if string match -qrv '.+\..+' $argv
+    echo "The argument '$argv' doesn't meet the requirements for URIs"
+    return 1
+  end
+  return 0
+end
+
+function __json_cache_add_uri_prefix
+  if string match -qrv '^https?://' $argv
+    echo "https://$argv"
+  else
+    echo "$argv"
   end
 end
 
