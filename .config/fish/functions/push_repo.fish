aafc5a20aba2ee80cf04df82e9b89093c5d86da2
Moisés Carvalho <moisescarv27@hotmail.com>
2020-04-13 04:23:47 -0300

    More quiet messages

--- .config/fish/functions/push_repo.fish
+++ .config/fish/functions/push_repo.fish
@@ -4,7 +4,7 @@ function push_repo
   set target_dir $argv[1]
 
   echo -n 'Pushing... '
-  if not g -C $target_dir push
+  if not g -C $target_dir push -q
       if not g -C $target_dir pull
           for f in (g -C $target_dir diff --name-only --diff-filter=U)
 	      vim $target_dir/$f
