8bf6a6f9fdc57d0d315a1c780de00c34b5cf7c24
Moisés Carvalho <moisescarv27@hotmail.com>
2020-04-13 03:48:58 -0300

    Implemented auto commit for tasks

--- .config/fish/functions/tasks.fish
+++ .config/fish/functions/tasks.fish
@@ -1,11 +1,12 @@
 #!/usr/bin/env fish
 
 function tasks -d "Manage personal tasks"
-  set -g tasks_file ~/Documents/Books/Others/tasks.json
+  set -g tasks_file ~/Documents/Books/Tasks/tasks.json
   if not __tasks_check_file; return 1; end
+  if not __tasks_check_json_formatting; return 3; end  
 
-  set options 'p/priority' 'l/low' 'n/normal' 'h/high' 'e/edit' 'd/delete' 'y/yes'
-  argparse -n 'Tasks' -x 'p,e,d' -x 'p,l,n,h' -x 'p,y' -X 2 $options -- $argv
+  set options 'p/priority' 'l/low' 'n/normal' 'h/high' 'e/edit' 'd/delete'
+  argparse -n 'Tasks' -x 'p,e,d' -x 'p,l,n,h' -x 'p,y' -x 'r,p,l,n,h,e,d' $options -- $argv
   if test $status -ne 0; return 2; end
 
   if set -q _flag_low; set -g selected_priority 'low'; end
@@ -15,6 +16,30 @@ function tasks -d "Manage personal tasks"
   if set -q _flag_edit; set -g selected_operation 'edit'; end
   if set -q _flag_delete; set -g selected_operation 'delete'; end
 
+  if set -q selected_operation
+    switch $selected_operation
+      case 'edit'
+        echo 'Edit'
+      case 'delete'
+        G
+    end
+  else if set -q selected_priority
+    if not count $argv > /dev/null
+      __tasks_print $selected_priority
+    else
+      __tasks_create "$argv" $selected_priority
+    end
+  else if set -q _flag_priority
+    __tasks_print 'low'
+    __tasks_print 'normal'
+    __tasks_print 'high'
+  else
+    if not count $argv > /dev/null
+      __tasks_print
+    else
+      __tasks_create "$argv" 'normal'
+    end
+  end
 
   #set -e tasks_file
   set -e selected_priority
@@ -25,7 +50,7 @@ end
 function __tasks_print
   if not count $argv > /dev/null
     set filter '[.tasks[]]'
-    echo "All tasks"
+    echo 'All tasks'
   else
     set filter "[.tasks[] | select(.priority == \"$argv\")]"
     echo "Tasks with $argv priority"
@@ -33,7 +58,12 @@ function __tasks_print
 
   set tasks_count (jq "$filter | length" $tasks_file)
 
+  if test $tasks_count -eq 0
+    echo "NO TASKS"
+    return
+  end
 
+  echo ' ID | DATE        | TASK'
   for i in (seq 0 (math "$tasks_count - 1"))
     set id (jq -r "$filter [$i].id" $tasks_file)
     set task (jq -r "$filter [$i].task" $tasks_file)
@@ -62,5 +92,40 @@ function __tasks_check_file
     return 1
   end
   return 0
+end
+
+function __tasks_create
+  set index (jq .next_index $tasks_file)
+  set date (date '+%d/%m %H:%M')
+  set tmp_file /tmp/(date +%N)
+
+  jq ".tasks += [{
+        id: $index,
+        task: \"$argv[1]\",
+        priority: \"$argv[2]\",
+        date: \"$date\"
+      }]" $tasks_file > $tmp_file
+  mv $tmp_file $tasks_file
+
+  jq ".next_index += 1" $tasks_file > $tmp_file
+  mv $tmp_file $tasks_file
+end
+
+function __tasks_check_json_formatting
+  if not jq . $tasks_file > /dev/null 2> /dev/null
+    echo 'The tasks file has JSON formatting errors.'
+    echo "Please check this file: $tasks_file"
+    return 1
+  end
+  return 0
+end
+
+functon __tasks_delete_all
+  echo '{"next_index": 0, "tasks": []}' | jq . > $tasks_file
+end
+
+function __tasks__commit_changes
+  g -C $tasks_file add -A
+  g -C $tasks_file commit -m $argv
 end
 
