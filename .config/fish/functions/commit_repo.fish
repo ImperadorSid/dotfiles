56cf7cfd0964a778b0545e79abd7c8bbb5999411
Moisés Carvalho <moisescarv27@hotmail.com>
2020-04-13 17:00:59 -0300

    Fix commit changes script prompt colors

--- .config/fish/functions/commit_repo.fish
+++ .config/fish/functions/commit_repo.fish
@@ -3,7 +3,7 @@
 function commit_repo
     set out_dir $argv[1]
     
-    read -p 'set_color green; echo -n \'Commit message\'; set_color normal; echo \': \'' msg
+    read -p 'set_color green; echo -n \'Commit message: \'; set_color normal' msg
     if test -n $msg
         g -C $out_dir add .
         g -C $out_dir commit -qm $msg
