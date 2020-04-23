ba5c033c4a59ebe9fe150f7f7468d9e1fe8900a4
Moisés Carvalho <moisescarv27@hotmail.com>
2020-04-23 02:44:16 -0300

    Add shebang

--- .config/fish/completions/repos.fish
+++ .config/fish/completions/repos.fish
@@ -1,4 +1,5 @@
+#!/usr/bin/env fish
 function __repos_completions_list
-  find $repositories -maxdepth 1 -name '*.repo' -printf '%f\tRepository: ' -exec sh -c "sed '/^#!/,\$d' {} | jq -r '.repo'" \;
+  find $repositories -maxdepth 1 -name '*.repo' -exec fish -c 'repo_metadata -c (basename \{})' \; -printf '%f\tRepository: ' -exec fish -c 'repo_metadata (basename {}) ".repo"' \;
 end
 complete -f -c repos -a '(__repos_completions_list)' -d 'List avaliable repositories scripts'
--- .config/fish/functions/repo_metadata.fish
+++ .config/fish/functions/repo_metadata.fish
@@ -1,10 +1,15 @@
 function repo_metadata
-  argparse -s 'n/null' -- $argv
+  set options 'n/null' 'c/check'
+  argparse -s -x 'n,c' $options -- $argv
+  test $status -ne 0; and return 2
+
   test ! -f $repositories/$argv[1]; and return 1
 
   set metadata (sed '/^#!/,$d' $repositories/$argv[1])
 
-  if set -q _flag_null
+  if set -q _flag_check
+    echo $metadata | jq -e 'has("repo") and has("type")' &> /dev/null
+  else if set -q _flag_null
     echo $metadata | jq -e $argv[3] $argv[2] > /dev/null
   else
     echo $metadata | jq -re $argv[3] $argv[2]
