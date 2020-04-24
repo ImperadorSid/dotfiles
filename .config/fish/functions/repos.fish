873d32e4af7db7be32b304d25781ba01e7179481
Moisés Carvalho <moisescarv27@hotmail.com>
2020-04-24 03:51:41 -0300

    Implement install function (missing create "Add to PATH" feature)

--- .config/fish/functions/repos.fish
+++ .config/fish/functions/repos.fish
@@ -6,16 +6,35 @@ function repos -d 'Manage repository downloads and script installations'
 
   set -g repo_file $argv[1]
   set -g repo_path "$repositories/$argv[1]"
+  alias meta 'repo_metadata $repo_file'
+  alias meta_quiet 'repo_metadata -n $repo_file'
 
-  if set -q _flag_create;
+  if set -q _flag_create
     echo 'Create'
   else if set -q _flag_edit
     echo 'Edit'
   end
 
   not __repos_check_file; and return 1
-  set repo_type repo_metadata $repo_file '.type'
-  __repos_unset
+
+  set -g repo_name (meta '.repo')
+  set -g repo_address (string match -qr '^https' $repo_name; and echo $repo_name; or echo "https://github.com/$repo_name")
+  set repo_type (meta '.type')
+
+  switch $repo_type
+    case 'backup'
+      echo 'Backup'
+    case 'install'
+      __repos_install "$_flag_only_download$_flag_only_install"
+    case 'release'
+      echo 'Release'
+    case *
+      echo 'Repo type is invalid'
+      __repos_cleanup_env
+      return 3
+  end
+
+  __repos_cleanup_env
   return 0
 end
 
@@ -28,12 +47,65 @@ function __repos_check_file
     return 0
   end
 
-  __repos_unset
+  __repos_cleanup_env
   return 1
 end
 
-function __repos_unset
+function __repos_cleanup_env
   set -e repo_file
   set -e repo_path
+  set -e repo_name
+  set -e repo_address
+  set -e repo_location
+
+  functions -e meta meta_quiet
+end
+
+function __repos_install
+  __repos_find_location
+
+  if test "$argv[1]" != '-i'
+    g clone $repo_address $repo_location
+  end
+
+  if test "$argv[1]" != '-d'
+    set current_location $pwd
+    cd $repo_location; or return 1
+
+    __repos_script | bash
+    meta_quiet 'has("links")'; and __repos_links
+    meta_quiet 'has("path_folders")'; and __repos_path_folders
+
+    cd $current_location
+  end
+end
+
+function __repos_script
+  sed -n '/^#!/,$p' $repo_path
+end
+
+function __repos_links
+  set links_count (meta '.links | length')
+  for i in (seq 0 (math "$links_count - 1"))
+    set destination (meta ".links[$i].destination" | sed -r 's|^~|/home/impsid|')
+    test "$destination" = 'null'; and set destination '/home/impsid/.local/bin'
+    mkdir -p $destination
+
+    echo -e "\nCreating links in $destination"
+
+    set files_count (meta ".links[$i].files | length")
+    for j in (seq 0 (math "$files_count - 1"))
+      set file_name (meta ".links[$i].files[$j]")
+      ln -sf $file_name $destination
+      echo "Link to $file_name created"
+    end
+  end
+end
+
+function __repos_find_location
+  set -g repo_location (meta '.location' | sed -r 's|^~|/home/impsid|')
+  if test "$repo_location" = 'null'
+    set repo_location $repositories/(string replace -r '(.*)\.repo$' '$1' $repo_file)
+  end
 end
 
