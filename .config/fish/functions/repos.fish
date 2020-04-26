5c4edcf15570e516b9360cd2eca9e1fc32a281e6
Moisés Carvalho <moisescarv27@hotmail.com>
2020-04-25 21:26:42 -0300

    Create and edit (partial) completed

--- .config/fish/functions/repos.fish
+++ .config/fish/functions/repos.fish
@@ -1,35 +1,36 @@
 #!/usr/bin/env fish
 function repos -d 'Manage repository downloads and script installations'
-  set options 'e/edit' 'c/create' 'i/only-install' 'd/only-download' 'f/force-clear'
-  argparse -n 'Repository Management' -x 'c,e,i,d' -x 'f,c,e' -X 2 $options -- $argv
+  set options 'e/edit' 'c/create' 'i/only-install' 'd/only-download' 'f/force-clear' 'm/metadata' 's/script'
+  argparse -n 'Repository Management' -x 'c,e,i,d' -x 'f,e' -x 'm,s,c,i,d,f' -X 2 $options -- $argv
   test $status -ne 0; and return 2
 
   set -g repo_file $argv[1]
-  set -g repo_path "$repositories/$argv[1]"
-  alias meta 'repo_metadata $repo_file'
-  alias meta_quiet 'repo_metadata -n $repo_file'
+  set -g repo_path "$repositories/$repo_file"
 
   if set -q _flag_create
-    echo 'Create'
+    __repos_create "$argv[2]" $_flag_f; or return 5
   else if set -q _flag_edit
-    echo 'Edit'
-  end
+    __repos_edit
+  else
+    alias meta 'repo_metadata $repo_file'
+    alias meta_quiet 'repo_metadata -n $repo_file'
 
-  not __repos_check_file; and return 1
+    __repos_check_file; or return 1
 
-  set -g repo_name (meta '.repo')
-  __repos_get_address
-  set repo_type (meta '.type')
+    set -g repo_name (meta '.repo')
+    __repos_get_address
+    set repo_type (meta '.type')
 
-  switch $repo_type
-    case 'backup'
-      echo 'Backup'
-    case 'clone' 'release'
-      __repos_clone_release "$_flag_i$_flag_d" $_flag_f; or return 4
-    case *
-      echo 'Repo type is invalid'
-      __repos_cleanup_env
-      return 3
+    switch $repo_type
+      case 'backup'
+        echo 'Backup'
+      case 'clone' 'release'
+        __repos_clone_release "$_flag_i$_flag_d" $_flag_f; or return 4
+      case *
+        echo 'Repo type is invalid'
+        __repos_cleanup_env
+        return 3
+    end
   end
 
   __repos_cleanup_env
@@ -49,15 +50,13 @@ function __repos_check_file
   return 1
 end
 
-function __repos_cleanup_env
-  set -e repo_file
-  set -e repo_path
-  set -e repo_name
-  set -e repo_address
-  set -e repo_location
-  set -e FILE_NAMES
+function __repos_get_address
+  if string match -qr '^https' $repo_name
+    set -g repo_address $repo_name
+  else
+    set -g repo_address "https://github.com/$repo_name"
+  end
 
-  functions -e meta meta_quiet
 end
 
 function __repos_clone_release
@@ -70,40 +69,10 @@ function __repos_clone_release
   return 0
 end
 
-function __repos_script
-  sed -n '/^#!/,$p' $repo_path
-end
-
-function __repos_links
-  set links_count (meta '.links | length')
-  for i in (seq 0 (math "$links_count - 1"))
-    set destination (meta ".links[$i].destination" | sed -r 's|^~|/home/impsid|')
-    test "$destination" = 'null'; and set destination '/home/impsid/.local/bin'
-    mkdir -p $destination
-
-    echo -e "\nCreating links in $destination"
-
-    for f in (meta ".links[$i].files[]")
-      ln -sf $f $destination
-      echo "Link to $f created"
-    end
-  end
-end
-
 function __repos_find_location
   set -g repo_location (meta '.location' | sed -r 's|^~|/home/impsid|')
   if test "$repo_location" = 'null'
     set repo_location $repositories/(string replace -r '(.*)\.repo$' '$1' $repo_file)
-  end
-end
-
-function __repos_path_folders
-  echo 
-  for f in (meta '.path_folders[]')
-    if not contains $repo_location/$f $fish_user_paths
-      echo "Adding folder \"$f\" to PATH"
-      set -p fish_user_paths $repo_location/$f
-    end
   end
 end
 
@@ -120,17 +89,6 @@ function __repos_download
   end
 
   return 0
-end
-
-function __repos_install
-  set current_location $pwd
-  cd $repo_location; or return 1
-
-  __repos_script | bash
-  meta_quiet 'has("links")'; and __repos_links
-  meta_quiet 'has("path_folders")'; and __repos_path_folders
-
-  cd $current_location
 end
 
 function __repos_get_files
@@ -161,15 +119,6 @@ function __repos_get_files
   return 0
 end
 
-function __repos_get_address
-  if string match -qr '^https' $repo_name
-    set -g repo_address $repo_name
-  else
-    set -g repo_address "https://github.com/$repo_name"
-  end
-
-end
-
 function __repos_name_formatting
   if string match -qrv '^[\w-]+/[\w-]+$' $repo_name
     echo 'To downloads releases from GitHub, the "repo" field must be formatted as <user>/<repo-name>'
@@ -186,5 +135,104 @@ function __repos_tag_assets
   else
     json_cache $argv[2] "$uri_prefix?per_page=100" | jq ".[] | select(.tag_name == \"$argv[1]\").assets[]"
   end
+end
+
+function __repos_install
+  set current_location $pwd
+  cd $repo_location; or return 1
+
+  __repos_script | bash
+  meta_quiet 'has("links")'; and __repos_links
+  meta_quiet 'has("path_folders")'; and __repos_path_folders
+
+  cd $current_location
+end
+
+function __repos_script
+  sed -n '/^#!/,$p' $repo_path
+end
+
+function __repos_links
+  set links_count (meta '.links | length')
+  for i in (seq 0 (math "$links_count - 1"))
+    set destination (meta ".links[$i].destination" | sed -r 's|^~|/home/impsid|')
+    test "$destination" = 'null'; and set destination '/home/impsid/.local/bin'
+    mkdir -p $destination
+
+    echo -e "\nCreating links in $destination"
+
+    for f in (meta ".links[$i].files[]")
+      ln -sf $f $destination
+      echo "Link to $f created"
+    end
+  end
+end
+
+function __repos_path_folders
+  echo 
+  for f in (meta '.path_folders[]')
+    if not contains $repo_location/$f $fish_user_paths
+      echo "Adding folder \"$f\" to PATH"
+      set -p fish_user_paths $repo_location/$f
+    end
+  end
+end
+
+function __repos_cleanup_env
+  set -e repo_file
+  set -e repo_path
+  set -e repo_name
+  set -e repo_address
+  set -e repo_location
+  set -e FILE_NAMES
+
+  functions -e meta meta_quiet
+end
+
+function __repos_create
+
+  set repo_path $repo_path.repo
+  if test -f "$repo_path" -a 'x-f' != "x$argv[2]"
+    echo 'Repository file already exists'
+    return 1
+  end
+
+  set type '"type": ""'
+  set repo '"repo": "/"'
+  set location '"location": ""'
+  set links '"links": [{"destination": "", "files": [""]}]'
+  set path_folders '"path_folders": [""]'
+  set targets '"targets": [{"tag": "", "files": [""]}]'
+
+  switch $argv[1]
+    case 'backup'
+      set template "{$type, $repo, $location}"
+    case 'clone'
+      set template "{$type, $repo, $location, $links, $path_folders}"
+    case 'release'
+      set template "{$type, $repo, $location, $links, $path_folders, $targets}"
+    case '*'
+      echo "Type \"$argv\" is not valid"
+      __repos_cleanup_env
+      return 1
+  end
+  
+  echo $template | jq '.' > $repo_path
+  echo '#!/usr/bin/env bash' >> $repo_path
+
+  echo "Repository $repo_file ($argv) created"
+  return 0
+end
+
+function __repos_edit
+  if test -f $repo_path
+    vim -c 'set filetype=json' $repo_path
+  else
+    echo "Repo file $repo_file doesn't exist"
+    return 1
+  end
+
+  __repos_cleanup_env
+  return 0
 end
 
