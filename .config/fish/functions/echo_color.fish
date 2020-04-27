593888389d35b0dce4d21b4d42296b45c7b57ea1
Moisés Carvalho <moisescarv27@hotmail.com>
2020-04-27 20:35:43 -0300

    Insert double dash to separate flags from strings

--- .config/fish/functions/echo_color.fish
+++ .config/fish/functions/echo_color.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 function echo_color
-  string length -q $argv[1]; or set argv[1] 'normal'
+  string length -q -- $argv[1]; or set argv[1] 'normal'
   set_color (string split ' ' -- $argv[1]); or return 1
 
   echo $argv[2..-1]
