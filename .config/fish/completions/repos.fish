60dcd91dd4abfb8f137661de30d02ee8905b9d36
Moisés Carvalho <moisescarv27@hotmail.com>
2020-04-27 16:05:37 -0300

    Improve output messages

--- .config/fish/completions/repos.fish
+++ .config/fish/completions/repos.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 function __repos_completions_list
-  find $repositories -maxdepth 1 -name '*.repo' -exec fish -c 'repo_metadata -c (basename \{})' \; -printf '%f\tRepository: ' -exec fish -c 'repo_metadata (basename {}) ".repo"' \;
+  find $repositories -maxdepth 1 -name '*.repo' -exec fish -c 'repo_metadata -c (basename "{}")' \; -printf '"%f"\tRepository: ' -exec fish -c 'repo_metadata (basename "{}") ".repo"' \;
 end
 complete -f -c repos -a '(__repos_completions_list)' -d 'List avaliable repositories scripts'
