161eeb3b17912b1f06dee8a528d72afa08cd7687
Moisés Carvalho <moisescarv27@hotmail.com>
2020-05-12 03:44:41 -0300

    Improve completion

--- .config/fish/completions/repos.fish
+++ .config/fish/completions/repos.fish
@@ -1,5 +1,12 @@
 #!/usr/bin/env fish
 function __repos_completions_list
-  find $repositories -maxdepth 1 -name '*.repo' -exec fish -c 'repo_metadata -c (basename "{}")' \; -printf '"%f"\tRepository: ' -exec fish -c 'repo_metadata (basename "{}") ".repo"' \;
+  fd -d 1 -e repo -x fish -c "repo_metadata -c {/}; or exit; echo -e '{/}\tRepository:' (repo_metadata {/} '.repo')" \; . $repositories
 end
 complete -f -c repos -a '(__repos_completions_list)' -d 'List avaliable repositories scripts'
+complete -f -c repos -s e -l 'edit' -d 'Edit a repo script'
+complete -f -c repos -s c -l 'create' -d 'Create a repo script'
+complete -f -c repos -s i -l 'only-install' -d 'Execute only install-related operations'
+complete -f -c repos -s d -l 'only-download' -d 'Execute only download-related operation'
+complete -f -c repos -s f -l 'force-clear' -d 'Clear JSON cache and delete old files'
+complete -f -c repos -s o -l 'open' -d 'Change into repo location and open with default file manager'
+complete -f -c repos -s O -l 'only-open' -d 'Similar to -o option, but don\'t execute any other operation'
