63d711051ac13c5bade0697acb1299479867bd37
Moisés Carvalho <moisescarv27@hotmail.com>
2020-05-13 13:20:15 -0300

    Add completions for "tasks" script

---
+++ .config/fish/completions/tasks.fish
@@ -0,0 +1,6 @@
+complete -f -c tasks -s p -l 'priority' -d 'Print tasks grouped by priority'
+complete -f -c tasks -s l -l 'low' -d 'CRUD for low priority tasks'
+complete -f -c tasks -s n -l 'normal' -d 'CRUD for normal priority tasks'
+complete -f -c tasks -s h -l 'high' -d 'CRUD for high priority tasks'
+complete -f -c tasks -s e -l 'edit' -d 'Edit a task'
+complete -f -c tasks -s d -l 'delete' -d 'Delete one or more tasks'
