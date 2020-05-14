62f74959714b3c8fce77a550d092ad63e9bb7941
Moisés Carvalho <moisescarv27@hotmail.com>
2020-05-14 02:16:39 -0300

    Add option to show what shell has been used

--- .config/fish/functions/personal_script.fish
+++ .config/fish/functions/personal_script.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 
 function personal_script
+  argparse 's/show-shell' -- $argv
   if not count $argv > /dev/null
     echo 'A task name must be passed as a parameter'
     return 4
@@ -21,21 +22,21 @@ function personal_script
   set script_full_path $scripts_dir/$script_name
 
   if string match -qr '\.fish$' $script_name
-    echo 'Using fish'
+    set -q _flag_show_shell; and echo 'Using fish'
     fish $script_full_path $argv[2..-1]
 
   else if string match -qr '\.(ba)?sh$' $script_name
-    echo 'Using bash'
+    set -q _flag_show_shell; and echo 'Using bash'
     bash $script_full_path $argv[2..-1]
 
   else
     set shell_executable (sed -nr '1 s/^#!(.*)/\1/p' $script_full_path)
 
     if count $shell_executable > /dev/null
-      echo "Using $shell_executable"
+      set -q _flag_show_shell; and echo "Using $shell_executable"
       eval "$shell_executable $script_full_path $argv[2..-1]"
     else
-      echo 'Trying running as a bash script'
+      set -q _flag_show_shell; and echo 'Trying running as a bash script'
       bash $script_full_path $argv[2..-1]
     end
   end
