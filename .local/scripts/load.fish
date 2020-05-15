2256aa75b12e7ab78d18c5c4ba58d1ba876669cc
Moisés Carvalho <moisescarv27@hotmail.com>
2020-05-15 04:26:14 -0300

    Break command inside for

--- .local/scripts/load.fish
+++ .local/scripts/load.fish
@@ -1,13 +1,18 @@
 #!/usr/bin/env fish
+function __load_stop -s USR1
+  set stop true
+end
+
+set stop false
 set states '\u2807' '\u280B' '\u2819' '\u2838' '\u2834' '\u2826'
-set delay 0.05
-count $argv > /dev/null; and set delay $argv
+count $argv > /dev/null; and set delay $argv; or set delay 0.075
 
 printf ' '
-while true
+while not $stop
   for i in (seq (count $states))
+    $stop; and break
     printf "\b$states[$i]"
     sleep $delay
   end
 end
-printf '\b'
+
