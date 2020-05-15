60e7e497d25ac51f7660219a126d23f2e85ad3dd
Moisés Carvalho <moisescarv27@hotmail.com>
2020-05-15 19:50:03 -0300

    Add loading screen

--- .config/fish/functions/push_repo.fish
+++ .config/fish/functions/push_repo.fish
@@ -4,14 +4,14 @@ function push_repo
   set target_dir $argv[1]
 
   echo -n 'Pushing... '
-  if not g -C $target_dir push -q
-      if not g -C $target_dir pull
+  if not loading g -C $target_dir push -q
+      if not loading g -C $target_dir pull
           for f in (g -C $target_dir diff --name-only --diff-filter=U)
 	      vim $target_dir/$f
           end
           commit_repo $target_dir
       end
-      g -C $target_dir push -q
+      loading g -C $target_dir push -q
   end
   echo 'complete'
 end
