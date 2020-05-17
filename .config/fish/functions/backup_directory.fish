c55b48bbe612761afb614558127e5c1cadf6332a
Moisés Carvalho <moisescarv27@hotmail.com>
2020-05-17 03:44:12 -0300

    Separate backup function in more specialized functions

--- .config/fish/functions/backup_directory.fish
+++ .config/fish/functions/backup_directory.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 function backup_directory
-  set options 'd/diff' 'r/restore' 'e/edit'
-  argparse -n 'Backup Directory' -N 2 -x 'd,r' $options -- $argv
+  set options 'd/diff' 'r/restore' 'e/edit' 'n/no-commit'
+  argparse -n 'Backup Directory' -N 2 -x 'd,r,e' -x 'd,r,n' $options -- $argv
   test "$status" -eq 0; or return 1
 
   if __backup_directory_init_variables $argv
@@ -10,7 +10,7 @@ function backup_directory
     else if set -q _flag_restore
       __backup_directory_restore
     else
-      __backup_directory_backup $_flag_edit
+      __backup_directory_backup "$_flag_edit" "$_flag_no_commit"
     end
   end
 
@@ -25,13 +25,15 @@ function __backup_directory_backup
     return 2
   end
 
-  set relative_path (realpath --relative-base=$target_dir $fd_path)
-  if test (string sub -s 1 -l 1 $relative_path) = '/'
-    echo_err "\"$fd_path\" isn't inside of \"$target_dir\""
-    return 3
-  end
+  __backup_directory_edit_copy $argv[1]
 
-  if test "$argv" = '-e'
+  __backup_directory_commit $argv[2]
+
+  return 0
+end
+
+function __backup_directory_edit_copy
+  if test "x$argv" = 'x-e'
     test -w "$fd_path"; and v $fd_path; or V $fd_path
   end
 
@@ -39,8 +41,16 @@ function __backup_directory_backup
   mkdir -p $destination_dir
 
   cp -r $fd_path $destination_dir
+end
 
-  return 0
+function __backup_directory_commit
+  if test "x$argv" != 'x-n'
+    if test ! -d "$repo_path/.git"
+      echo_err "Not a Git repository. Skipping commits..."
+    else
+      commit_repo $repo_path
+    end
+  end
 end
 
 function __backup_directory_diff
@@ -66,8 +76,21 @@ function __backup_directory_init_variables
       echo_err "\"$argv[3]\" doesn't exist"
       return 4
     end
-
     set -g fd_path $argv[3]
+    
+    __backup_directory_check_relative
+    return $status
+  end
+
+  return 0
+end
+
+function __backup_directory_check_relative
+  set -g relative_path (realpath --relative-base=$target_dir $fd_path)
+
+  if test (string sub -s 1 -l 1 $relative_path) = '/'
+    echo_err "\"$fd_path\" isn't inside of \"$target_dir\""
+    return 3
   end
 
   return 0
@@ -75,8 +98,11 @@ end
 
 function __backup_directory_unset_variables
   set -e target_dir
+  
   set -e repo_name
   set -e repo_path
+
   set -e fd_path
+  set -e relative_path
 end
 
