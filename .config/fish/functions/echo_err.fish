a3c08015601728969a0acd10c040a7967d1aadc6
Moisés Carvalho <moisescarv27@hotmail.com>
2020-05-18 02:00:18 -0300

    Replace "echo_err" alias for a function

---
+++ .config/fish/functions/echo_err.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+function echo_err
+  printf '%sERROR: %s' (set_color brred) (set_color normal)
+
+  if string match -qr '^\d+$' -- "$argv[-1]"
+    echo $argv[1..-2]
+    return $argv[-1]
+  else
+    echo $argv
+    return 1
+  end
+end
+
