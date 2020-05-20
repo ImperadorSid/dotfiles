cc2870b1dbf4f2f4a142015aa2e69a1b18ad27e7
Moisés Carvalho <moisescarv27@hotmail.com>
2020-05-20 20:37:35 -0300

    Update commit_repo and push_repo to more new recent scripts

--- .config/fish/functions/commit_repo.fish
+++ .config/fish/functions/commit_repo.fish
@@ -1,20 +1,26 @@
 #!/usr/bin/env fish
 
 function commit_repo
+  argparse -n 'n/no-push' -- $argv
+  test "$status" -eq 0; or return
+
   test -n "$argv"; and set repo_dir $argv; or set repo_dir $PWD
+  set current_directory $PWD
   $repo_dir
 
   set git_status (g status -s)
   if test -n "$git_status"
     read -p 'echo_color "green" -n "Commit message: "' message
-    test -n "$message"; and g add -A; and g commit -qm $message
+    test -n "$message"; and g add -A; and g commit -qm $message; or return
 
-    read -p 'echo_color "blue" -n "Make push? "; echo -n "[Y/n] "' choice
-    test "$choice" = 'n'; or push_repo $repo_dir
+    if not set -q _flag_no_push
+      read -p 'echo_color "blue" -n "Make push? "; echo -n "[Y/n] "' choice
+      test "$choice" = 'n'; or push_repo
+    end
   else
     echo 'Nothing to commit'
   end
 
-  cd -
+  $current_directory
 end
 
--- .config/fish/functions/push_repo.fish
+++ .config/fish/functions/push_repo.fish
@@ -1,17 +1,25 @@
 #!/usr/bin/env fish
 
 function push_repo
-  set target_dir $argv[1]
+  test -n "$argv"; and set target_dir $argv; or set target_dir $PWD
+  set current_directory $PWD
+  $target_dir
 
-  echo -n 'Pushing... '
-  if not loading g -C $target_dir push -q
-      if not loading g -C $target_dir pull
-          for f in (g -C $target_dir diff --name-only --diff-filter=U)
-	      vim $target_dir/$f
-          end
-          commit_repo $target_dir
-      end
-      loading g -C $target_dir push -q
+  if not __push_repo_make_push
+    if not loading g pull
+      v $target_dir/(g diff --name-only --diff-filter=U)
+      commit_repo
+    else
+      __push_repo_make_push
+    end
   end
-  echo 'complete'
+
+  $current_directory
 end
+
+function __push_repo_make_push
+  printf 'Pushing... '
+  loading g push -q
+  and echo 'complete'
+end
+
