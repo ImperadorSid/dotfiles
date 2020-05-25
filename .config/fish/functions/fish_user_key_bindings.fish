c0ec5ba18b4de98a2cd0750bfb7432e325553c74
Moisés Carvalho <moisescarv27@hotmail.com>
2020-05-25 02:29:21 -0300

    Improve ignore files

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,6 @@
 
 function fish_user_key_bindings
   bind -M insert \e, 'v /home/impsid/.config/fish/config.fish'
-  bind -M insert \e. 'v /home/impsid/.config/fish/functions'
+  bind -M insert \e. 'vf /home/impsid/.config/fish/functions'
 end
 
