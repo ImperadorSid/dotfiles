ca62d4f3e79a8137694feb5620a20e5979deaa05
Moisés Carvalho <moisescarv27@hotmail.com>
2020-06-01 18:41:23 -0300

    Modified output for no changes

--- .config/fish/functions/check_repos.fish
+++ .config/fish/functions/check_repos.fish
@@ -2,6 +2,7 @@
 
 function check_repos
   set current_directory $PWD
+  set have_changed false
   $repositories
 
   for r in *
@@ -11,7 +12,9 @@ function check_repos
     and printf '%s%s%s\n' (set_color green) "$r" (set_color normal)
     and git -C $r status
     and echo
+    and set have_changed true
   end
 
+  $have_changed; or echo_color 'blue' 'All repository are fully synched'
   $current_directory
 end
