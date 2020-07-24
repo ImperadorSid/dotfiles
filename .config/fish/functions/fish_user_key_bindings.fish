f66e05328d8fe608adfb40eb89546949b0842b63
Moisés Carvalho <moisescarv27@hotmail.com>
2020-07-23 22:39:51 -0300

    Add shortcut to jump to home folder

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -4,6 +4,7 @@ function fish_user_key_bindings
   bind -M insert \e, 'v /home/impsid/.config/fish/config.fish'
   bind -M insert \e. 'vf /home/impsid/.config/fish/functions'
   bind -M insert \em 'vf .'
+  bind -M insert \e0 'jump_list ~'
   bind -M insert \e1 'jump_list ~/Documents/Programs/Angular'
   bind -M insert \e2 'jump_list ~/Documents/Programs/NodeJS'
 end
